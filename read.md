7638b18753cf24ce3e9345cc31ee1938de76bd24
clearan <clearan77@gmail.com>
2022-03-13 11:42:33 +0800

    增加注释

---
+++ read.md
@@ -0,0 +1,9 @@
+```text
+node版本 14.17.5
+webpack 5.70.0
+vue: 3.2.31
+```
+
+- 以webpack.client.js为客户端入口打包，生成dist/client下的bundle文件， 为后续替换index.html中的<div id="app">xxx做准备
+- 以webpack.server.js为服务端入口打包，生成dist/server下的bundle文件，后续在服务端利用renderToString将要插入到id为app标签内的js转化成html代码
+- html来自于服务端入口打包所得，js，css等静态资源，来自于客户端打包所得
--- src/entry/server.entry.js
+++ src/entry/server.entry.js
@@ -4,6 +4,8 @@ import { createMemoryHistory } from 'vue-router'
 import createRouter from '../router.js'
 import App from '../demo.vue'
 
+// ，我们需要为每个请求创建一个新的 Vue 根实例服务端入口使用了一个默认导出，它是一个可以在每次渲染的过程中重复调用的函数
+// 服务端入口使用了一个默认导出，它是一个可以在每次渲染的过程中重复调用的函数
 export default function () {
     const app = createSSRApp(App)
     const router = createRouter(createMemoryHistory())
